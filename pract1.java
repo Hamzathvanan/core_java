7d68b6c2efc93f97e74a01a43859c8cbc8eb2214
Hamzathvanan <hamzathvanan@gmail.com>
2023-06-15 15:40:41 +0530

    Modified Main Method

--- pract1.java
+++ pract1.java
@@ -3,6 +3,14 @@ public class pract1{
     public static void  main(String[] args){
 
 	System.out.println("Hello World!");
+
+	List<Person> persons = new Persons().getPersons();
+
+	List<Person> myProfile = persons.stream()
+	    .filter(person -> person.getName().equals("Hamzath"))
+	    .colloect(Collectors.toList());
+
+	System.out.println("Profile: " + myProfile);
     }
 
 }
