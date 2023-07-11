fdd60548eca5ba82bce5ff6923da1877c67e9204
Hamzathvanan <hamzath@arogya.life>
2023-07-11 17:09:39 +0530

    Variables

--- Hello.java
+++ Hello.java
@@ -1,10 +1,20 @@
 public class Hello{
 
     public static void main (String[] args){
-      int num = 3;
-      int num2 = 5;
-      int result = num+num2;
-        System.out.println("Hello Hamzath!");
-        System.out.println("Total: " + result);
+
+      int num = 5;
+      byte by = 127;
+      short sh = 558;
+      long ln = 5458l;
+
+      float fl = 56.5f;
+      double dl = 56.54;
+
+      char c = 'k';
+
+      boolean bl = true;
+
+      String st = "Hello";
+
     }
 }
