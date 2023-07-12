dfc85a55515a5534a51edfd967700f229e0bad66
Hamzathvanan <hamzath@arogya.life>
2023-07-12 21:06:03 +0530

    Casting v2

--- TypeCasting.java
+++ TypeCasting.java
@@ -3,7 +3,17 @@ public class TypeCasting {
     public static void main (String[] args){
         byte b = 127;
         int a = b;
-        System.out.println("INT: " + a);
+        System.out.println("byte to int: " + a);
+
+
+        int c = 12;
+        byte d = (byte)c;
+        System.out.println("int to byte: " + d);
+
+
+        float e = 14.45f;
+        int f = (int)e;
+        System.out.println("float to int: " + f);
     }
 
 }
