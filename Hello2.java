66e17f1fd90b6973f5d160b70b9827a10756d560
Hamzathvanan <hamzathvanan@gmail.com>
2023-09-14 03:46:49 +0530

    Hello World

---
+++ Hello2.java
@@ -0,0 +1,8 @@
+public class Hello2 {
+
+    public static void main(String args[]) {
+        System.out.println("Hi Hamzath!!");
+        System.out.println(5+6);
+        System.out.println(5+8);
+    }
+}
